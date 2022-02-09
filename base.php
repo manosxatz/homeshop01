3f5e9adbc8c71f1769c1a8f193d7a4efe194573e
manosxatz <manos.xatz@yahoo.gr>
2022-02-09 14:30:27 +0200

    with bootstrap

--- base.php
+++ base.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="el">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Shop 01</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
 </head>
+
 <body>
     <header>
 
@@ -17,7 +20,8 @@
 
     </main>
     <footer>
-        
+
     </footer>
 </body>
+
 </html>
